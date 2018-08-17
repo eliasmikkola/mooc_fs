95fd66ca86ad2dd6ce88039ae162030119b6ea8a
Elias Mikkola <elias.mikkola1@gmail.com>
2018-08-17 17:22:00 +0300

    renamed folder for 1.1-1.2

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 
 
 
-const Button = (props) => ( <button onClick={props.handler}>{props.title}</button>)
-    
+const Button = (props) => (<button onClick={props.handler}>{props.title}</button>)
+
 const Header = (props) => (
     <h1>{props.title}</h1>
 )
@@ -16,84 +16,84 @@ const Statistic = (props) => (
 )
 
 const Statistics = (props) => {
-    if(props.total > 0){
+    if (props.total > 0) {
         return (
             <table>
-            <tbody>
-            {
-                props.stats.map(n => {
-                    return (
-                        <Statistic title={n.title} stat={n.stat} key={n.title}/>
-                    )
-                })
-            }
-            </tbody>
-        </table>)
-        
+                <tbody>
+                    {
+                        props.stats.map(n => {
+                            return (
+                                <Statistic title={n.title} stat={n.stat} key={n.title} />
+                            )
+                        })
+                    }
+                </tbody>
+            </table>)
+
     } else {
         return <p>ei yhtään palautatte annettu</p>
-    }   
+    }
 }
 
 class App extends React.Component {
     constructor(props) {
-      super(props)
-      this.state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        total: 0
-      }
+        super(props)
+        this.state = {
+            good: 0,
+            neutral: 0,
+            bad: 0,
+            total: 0
+        }
     }
-    
-    
+
+
     vote = (newState) => {
         newState["total"] = this.state.total + 1
         this.setState(newState)
     }
-    
-  
+
+
     render() {
-        const median = Math.round( (this.state.total > 0 ? (this.state.good + (this.state.bad * -1) ) / this.state.total : 0) * 10 ) / 10
-        const positive = Math.round(100 * (this.state.total > 0 ? this.state.good  / this.state.total : 0) * 10 ) / 10 +"%"
-        
+        const median = Math.round((this.state.total > 0 ? (this.state.good + (this.state.bad * -1)) / this.state.total : 0) * 10) / 10
+        const positive = Math.round(100 * (this.state.total > 0 ? this.state.good / this.state.total : 0) * 10) / 10 + "%"
+
         const stats = [
             {
                 title: "hyvä",
-                stat:this.state.good
+                stat: this.state.good
             },
             {
                 title: "neutraali",
-                stat:this.state.neutral
+                stat: this.state.neutral
             },
             {
                 title: "huono",
-                stat:this.state.bad
+                stat: this.state.bad
             },
             {
                 title: "keskiarvo",
-                stat:median
+                stat: median
             },
             {
                 title: "positiivista",
-                stat:positive
+                stat: positive
             }
         ]
         return (
             <div>
                 <div>
-                    <Header title="Anna palautetta"/>
-                    <Button handler={() => this.vote({good:this.state.good +1})} title={"hyvä"} />
-                    <Button handler={() => this.vote({neutral:this.state.neutral +1})} title={"neutraali"} />
-                    <Button handler={() => this.vote({bad:this.state.bad +1})} title={"huono"} />
-                    <Header title="Statistiikka"/>
-                    <Statistics stats={stats} total={this.state.total}/>
+                    <Header title="Anna palautetta" />
+                    <Button handler={() => this.vote({ good: this.state.good + 1 })} title={"hyvä"} />
+                    <Button handler={() => this.vote({ neutral: this.state.neutral + 1 })} title={"neutraali"} />
+                    <Button handler={() => this.vote({ bad: this.state.bad + 1 })} title={"huono"} />
+                    <Header title="Statistiikka" />
+                    <Statistics stats={stats} total={this.state.total} />
                 </div>
             </div>
         )
     }
-  }
-  ReactDOM.render(
+}
+ReactDOM.render(
     <App />,
     document.getElementById('root')
-  )
+)
